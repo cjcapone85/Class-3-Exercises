f6a2944c1925daa9590520635323030206f5965f
cjcapone85 <99698474+cjcapone85@users.noreply.github.com>
2022-02-21 20:04:13 -0600

    Class 3
    Class 3 Ex 1

---
+++ jsc-3-1.js
@@ -0,0 +1,18 @@
+function printString(message) {
+    console.log(`This is your ${message}`);
+
+}
+const message = ("potato");
+
+printString(message);
+
+
+
+
+
+
+
+//- [ ] Create a function
+//- [ ] Have that function take in an argument
+//- [ ] Print a statement that uses the variable along with some other text
+
