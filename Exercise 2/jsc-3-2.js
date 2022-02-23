d926625cea823bfdfd534cd346e743a7720e945e
cjcapone85 <99698474+cjcapone85@users.noreply.github.com>
2022-02-23 11:41:39 -0600

    Class 3 Exercises

---
+++ Exercise 2/jsc-3-2.js
@@ -0,0 +1,25 @@
+
+const user = {
+    name: "Chad",
+    age: 36,
+    occupation: "Electrician"
+
+};
+
+
+
+function printUserInfo(message = user) {
+    console.log(message);
+};
+
+//printUserInfo();
+
+
+//change to a string?
+
+
+// **Aim**: Create an application that, on button click, runs a function that prints out a users name, age, and occupation
+
+// - [ ] Create the HTML with a button
+// - [ ] Create a function that takes in a _user_ object
+// - [ ] Print each item in the user object out in a string to the console
